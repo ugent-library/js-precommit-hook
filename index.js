8910bd9d11da230f543c09f327499749e03ba35b
Koen Verheyen <verheyen.koen@gmail.com>
2022-09-14 08:56:40 +0200

    Converted to module (and removed esm)

--- index.js
+++ index.js
@@ -1,4 +1,47 @@
-#! /usr/bin/env node
+import { exec } from 'promisify-child-process'
+import path from 'path'
+import fs from 'fs'
+import 'colors'
 
-require = require('esm')(module)
-module.exports = require('./precommit.js')
+async function run() {
+  const re = /(debugger|\.only|\.skip)/
+
+  let { stdout } = await exec('git diff --cached --name-only --diff-filter=ACM')
+  if (stdout) {
+    const files = stdout.trim().split('\n')
+
+    for (let file of files) {
+      const content = fs.readFileSync(path.resolve(process.cwd(), file), 'utf-8')
+
+      const m = re.exec(content)
+      if (m) {
+        let matchIndex = m.index,
+          matchLine = 1
+        for (let line of content.split('\n')) {
+          if (line.length > matchIndex) {
+            break
+          }
+
+          matchIndex -= line.length + 1 // also count the line feed
+          matchLine++
+        }
+
+        console.error(
+          'Commit rejected:'.bgRed.bold,
+          'Found',
+          `"${m[1]}"`.yellow.bold,
+          'in file',
+          file.yellow.bold,
+          'at line',
+          matchLine.toString().yellow.bold,
+          'position',
+          (matchIndex + 1).toString().yellow.bold
+        )
+
+        process.exit(1)
+      }
+    }
+  }
+}
+
+run()
